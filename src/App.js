81c916a97e71ddd0c100001aba6939ea66b50b83
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-10-11 21:37:22 -0300

    Botões de voltas atualizado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 // componente para mostras voltas
 const MostraVoltas = (props) => {
@@ -21,19 +21,41 @@ const MostraTempo = (props) => {
 }
 
 // componente para mostrar botão
-const Button = (props) => <button>{props.text}</button>
+const Button = (props) => <button onClick={props.onclick}>{props.text}</button>
 
 function App() {
+  // definindo numero de voltas
+  const [numVoltas, setNumVoltas] = useState(14)
+  //definindo o tempo
+  const [tempo, setTempo] = useState(0)
+
+  // será atualizado apenas na primeira vez
+  useEffect(() => {
+    setInterval(() => {
+      
+    }, 1000)
+  }, [])
+
+  // incrementando o numero de voltas
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+  // decrementando o numero de voltas
+  const decrement = () => {
+    setNumVoltas(numVoltas - 1)
+  }
+  
   return (
     <div>
       <MostraVoltas voltas='14' />
-      <Button text='+'/>
-      <Button text='-'/>
-      <MostraTempo tempo='01:30'/>
+      <Button text='+' onclik={increment}/>
+      <Button text='-' onclick={decrement}/>
+      <MostraTempo tempo={tempo}/>
       <Button text='Iniciar'/>
       <Button text='Reiniciar'/>
     </div>
   );
 }
+
 
 export default App
