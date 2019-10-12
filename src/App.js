9d75b46f2a1e6072196a56fdd61c6d71bb25330a
JuarezJunior256 <juarezjunior.live@gmail.com>
2019-10-11 22:54:43 -0300

    Criando funcionalidades no timer

--- src/App.js
+++ src/App.js
@@ -12,29 +12,49 @@ const MostraVoltas = (props) => {
 
 // componente para mostrar tempo
 const MostraTempo = (props) => {
+  const tempo = props.tempo
+  const minutos =  Math.round(tempo / 60) // obtendo os minutos
+  const segundos = tempo % 60 // obtendo os segundos
+  const minutosStr = minutos < 10 ? '0' + minutos : minutos // formatação para mostrar dois algorismo
+  const segundosStr = segundos < 10 ? '0' + segundos : segundos // formatação para mostrar dois algorismo
+
   return (
     <p>
-      {props.tempo}<br />
+      {`${minutosStr} : ${segundosStr}`}<br />
       Tempo médio por volta
     </p>
   )
 }
 
 // componente para mostrar botão
-const Button = (props) => <button onClick={props.onclick}>{props.text}</button>
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>
 
 function App() {
   // definindo numero de voltas
   const [numVoltas, setNumVoltas] = useState(14)
+  // definindo booleando para running do tempo
+  const [running, setRunning] = useState(false)
   //definindo o tempo
   const [tempo, setTempo] = useState(0)
 
   // será atualizado apenas na primeira vez
   useEffect(() => {
-    setInterval(() => {
-      
-    }, 1000)
-  }, [])
+    let timer = null // variavel que irá receber uma instancia do setInterval
+    if (running) { 
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if (timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [running])
+  
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
 
   // incrementando o numero de voltas
   const increment = () => {
@@ -44,15 +64,24 @@ function App() {
   const decrement = () => {
     setNumVoltas(numVoltas - 1)
   }
+
+  // resetar o timer
+  const reset = () => {
+    setNumVoltas(0)
+    setTempo(0)
+  }
   
   return (
     <div>
-      <MostraVoltas voltas='14' />
-      <Button text='+' onclik={increment}/>
-      <Button text='-' onclick={decrement}/>
-      <MostraTempo tempo={tempo}/>
-      <Button text='Iniciar'/>
-      <Button text='Reiniciar'/>
+      <MostraVoltas voltas={numVoltas} />
+      <Button text='+' onClick={increment}/>
+      <Button text='-' onClick={decrement}/>
+      {
+        numVoltas > 0 &&
+        <MostraTempo tempo={tempo/numVoltas}/>
+      }
+      <Button onClick={toggleRunning} text='Iniciar'/>
+      <Button onClick={reset} text='Reiniciar'/>
     </div>
   );
 }
